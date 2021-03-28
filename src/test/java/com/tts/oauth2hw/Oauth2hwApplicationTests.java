f25d8330abe31a987676927a16c18d1536cf407e
angela-wheatley <79345893+angela-wheatley@users.noreply.github.com>
2021-03-28 14:28:32 -0500

    first commit of project

---
+++ src/test/java/com/tts/oauth2hw/Oauth2hwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.oauth2hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2hwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
